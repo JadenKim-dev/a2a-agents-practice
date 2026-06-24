baeb1fa36379e034df8064ca50cfafa6cb3c038b
JadenKim-dev <jaden.kim5179@gmail.com>
2026-06-24 23:38:58 +0900

    feat: A2A AgentCard 빌더 추가

---
+++ tests/test_agent_card.py
@@ -0,0 +1,27 @@
+from common.agent_card import build_agent_card
+
+
+def test_agent_card_url_is_in_supported_interfaces():
+    # given
+    name = "research"
+    url = "http://127.0.0.1:9001/"
+
+    # when
+    card = build_agent_card(
+        name=name,
+        description="Researches topics",
+        url=url,
+        skill_id="research",
+        skill_name="Research",
+        skill_description="Find information on a topic",
+        skill_tags=["research"],
+    )
+
+    # then
+    assert card.name == name
+    assert len(card.supported_interfaces) == 1
+    assert card.supported_interfaces[0].url == url
+    assert card.supported_interfaces[0].protocol_binding == "JSONRPC"
+    assert len(card.skills) == 1
+    assert card.skills[0].id == "research"
+    assert card.default_input_modes == ["text"]
